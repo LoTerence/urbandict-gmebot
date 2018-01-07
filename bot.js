aa16c7b6287860270ae34688c2240fe3f1cd0833
Terence <terencelomoa@gmail.com>
2018-01-07 16:54:08 -0500

    weird fix

--- bot.js
+++ bot.js
@@ -15,15 +15,15 @@ function respond() {
       botRegex = /^\/cool guy$/;
       reqText = request.text;
 
-  if(reqText && botRegex.test(reqText)) {
+  /* if(reqText && botRegex.test(reqText)) {
     this.res.writeHead(200);
     postMessage( cool() );
     this.res.end();
-  } /* else if(reqText && ( (^\/urbanDict /).test(reqText) ) ){
+  } else */ if(reqText && ( (^\/urbanDict /).test(reqText) ) ){
     this.res.writeHead(200);
     postMessage( urbanDict(reqText.slice(11)) );
     this.res.end;
-  }*/ else {
+  } else {
     console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
